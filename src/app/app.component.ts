2e1e2cdf6ded8ae08bed7536fde5e3da171511ef
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2021-02-02 10:29:43 +0100

    New components add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-movies';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.moviesInTheaters = [{
+      title: 'Spider-Man',
+      releaseDate: new Date(),
+      price: 1400.99
+     },
+     {
+      title: 'Moana',
+      releaseDate: new Date(),
+      price: 300.99
+     }
+    ];
+    this.moviesFutureReleases=[{
+      title: 'Avengers',
+      releaseDate: new Date(),
+      price: 456.99
+     },
+     {
+      title: 'Toy Story',
+      releaseDate: new Date('2021-02-24'),
+      price: 789.99
+     }
+    ];
+    
+  }
+ moviesInTheaters;
+ moviesFutureReleases;
 }
---
+++ src/app/utilities/generic-list/generic-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-list',
+  templateUrl: './generic-list.component.html',
+  styleUrls: ['./generic-list.component.css']
+})
+export class GenericListComponent implements OnInit {
+
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.list.length > 0);
+  }
+  @Input()
+  list;
+}
